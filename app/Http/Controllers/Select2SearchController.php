6f315f9a0985cd43e5d8b17f2b7136a2948d210e
baltazar-christian <baltazarchristian49@gmail.com>
2022-12-27 11:22:40 +0300

    first commit

---
+++ app/Http/Controllers/Select2SearchController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class Select2SearchController extends Controller
+{
+    public function index()
+
+    {
+
+    	return view('home');
+
+    }
+
+
+    public function selectSearch(Request $request)
+
+    {
+
+    	$movies = [];
+
+
+        if($request->has('q')){
+
+            $search = $request->q;
+
+            $movies =Movie::select("id", "name")
+
+            		->where('name', 'LIKE', "%$search%")
+
+            		->get();
+
+        }
+
+        return response()->json($movies);
+
+    }
+}
